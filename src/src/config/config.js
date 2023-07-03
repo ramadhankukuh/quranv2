6ebf82d22d6b708a6b386067d2c2b8a42f9bde1c
ramadhankukuh <ramadhankukuh9@gmail.com>
2023-07-03 18:00:28 +0700

    upd

---
+++ src/src/config/config.js
@@ -0,0 +1,11 @@
+import Greetings from "../pages/Home/Greetings";
+
+const config = {
+  site: {
+    name: <Greetings />,
+    foot: "ramadhankukuh by renova",
+    github: "https://ramadhankukuh.my.id",
+  },
+};
+
+export default config;
