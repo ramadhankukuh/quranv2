4592a11eb25a48c3c2062b3bd4d8df0f49bb9feb
Kuh <73922679+ramadhankukuh@users.noreply.github.com>
2023-07-03 17:50:05 +0700

    Add files via upload

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,10 @@
+import Greetings from "../pages/Home/Greetings";
+
 const config = {
   site: {
-    name: "Muslimin",
-    github: "https://github.com/renomureza/muslimin",
+    name: <Greetings />,
+    foot: "ramadhankukuh by renova",
+    github: "https://ramadhankukuh.my.id",
   },
 };
 
